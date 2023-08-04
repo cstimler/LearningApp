3ab68bc2fe79bd1c198adaa28d455115f8170882
cstimler <charles.ces@gmail.com>
2023-08-04 00:54:43 -0700

    Initial Commit

---
+++ Chris Module 5 L1 LearningApp/Chris_Module_5_L1_LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chris_Module_5_L1_LearningAppApp.swift
+//  Chris Module 5 L1 LearningApp
+//
+//  Created by October2022 on 8/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Chris_Module_5_L1_LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
